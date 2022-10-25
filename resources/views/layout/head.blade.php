bcc82bc94e2c954c71e28d6255c03e1d903bcd39
ReXooGen <ajulian.fernando123@gmail.com>
2022-10-25 10:50:01 +0700

    update fontawesome

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -9,7 +9,7 @@
    
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset ('template/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/732407f0ee.js" crossorigin="anonymous"></script>
     <link
         href="{{asset ('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i')}}"
         rel="stylesheet">
